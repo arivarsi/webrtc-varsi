5eaad83c5129dd094591324b400ea376e15e3d63
AriVarsi-Lean <everdamvarsi@gmail.com>
2020-06-29 04:33:58 +0200

    Adding provfile for heroku deployment

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ var http = require('http');
 var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8080;
+}
+
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(port);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
